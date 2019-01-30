da096b4672f68309435345346509a5f8bf4b4808
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-30 16:06:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(file_name)
     #"Michael Jackson - Balck or White - Pop.mp3"
     artist_and_song = file_name.split(" - ")
-    artist = Artist.new(artist_and_song[0]).save
+    artist = Artist.new(artist_and_song[0])
     song = self.new(artist_and_song[1])
     song.artist = artist.save
     artist.add_song(song)
