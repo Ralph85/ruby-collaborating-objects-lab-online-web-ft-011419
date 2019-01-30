ef3152d96492803b62762e1871503cd16ed94737
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-30 15:55:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file_name)
+    #"Michael Jackson - Balck or White - Pop.mp3"
+    artist_and_song = file_name.split(" - ")
+    artist = Artist.new(artist_and_song[0])
+    song = self.new(artist_and_song[1])
+    song.artist = artist.save
+    artist.add_song(song)
+    song
+  end
+
+  def artist=(artist)
+    song.artist = artist
+    artist.add_song(self)
+  end
+end
